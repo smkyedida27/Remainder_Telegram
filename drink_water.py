290dff4fcbfb18bbb3542dd7ff90f84b66fccb0c
Sri Manikanta <srimanikantayedida18@gmail.com>
2026-05-11 12:16:42 +0530

    telegram water bot

---
+++ drink_water.py
@@ -0,0 +1,19 @@
+import asyncio
+from telegram import Bot
+
+TOKEN = "8798118154:AAFHLysUdYJH03UUmW3y8LEDbzOBSAfYwXQ"
+CHAT_ID = "5336869933"
+
+bot = Bot(token=TOKEN)
+
+async def water_reminder():
+    while True:
+        await bot.send_message(
+            chat_id=CHAT_ID,
+            text="💧 Water Taagu"
+        )
+
+        # 1.5 hours
+        await asyncio.sleep(5400)
+
+asyncio.run(water_reminder())
